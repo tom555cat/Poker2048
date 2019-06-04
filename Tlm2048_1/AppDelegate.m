34602269e2a9e5ef5add06dd703b4b0b3e88406d
Tong Leiming <tongleiming@daling.com>
2019-06-04 16:31:26 +0800

    修改图标。

--- Tlm2048_1/AppDelegate.m
+++ Tlm2048_1/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "XCHostUtil.h"
+#import <UMCommon/UMCommon.h>
 
 @interface AppDelegate ()
 @property (strong, nonatomic) NSManagedObjectContext *achivementDatabaseContext;
@@ -18,9 +19,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-        
-    // 启动界面等待1分钟
-    [NSThread sleepForTimeInterval:1];
+    
+    [UMConfigure setEncryptEnabled:YES];
+    [UMConfigure setLogEnabled:YES];
+    [UMConfigure initWithAppkey:@"5cf627c70cafb2737f00127d" channel:@"App Store"];
     
     // 获取ip地址
     [XCHostUtil getIPv4AddressFromHost:@"www.baidu.com" completion:^(NSString * _Nonnull address) {
